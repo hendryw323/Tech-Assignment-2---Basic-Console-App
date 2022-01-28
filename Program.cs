ce619f64b11e83a0c68a0abd4656d93622d32126
wiley <wiley@DESKTOP-ICJH56T>
2022-01-28 13:09:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Programmer: Wiley Hendry
+// ISM 4300
+// Tech Assignment 2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yesterday is history, tomorrow is a mystery, but today is a gift.  That's why they call it the present.");
+            Console.WriteLine("Submitted by Wiley Hendry");
+        }
+    }
+}
